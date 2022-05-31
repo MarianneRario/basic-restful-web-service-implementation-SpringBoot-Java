4957617127d4e1840ba207ba6c8a1f85e184fed0
Marianne Rario <mariannecastillorario27@gmail.com>
2022-05-31 20:24:54 +0800

    implement api versioning using Header and Content Negotiation Approach

--- src/main/java/com/rariom/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/rariom/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -9,13 +9,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonVersioningController {
 
 
+    // path versioning
     @GetMapping(path = "/v1")
     public PersonV1 personV1(){
         return new PersonV1("Marianne V1");
     }
 
-    @GetMapping(path = "/v2")
-    public PersonV2 personV2(){
+    // params versioning
+    @GetMapping(value = "/param", params = "version=1")
+    public PersonV1 paramPersonV1(){
+        return new PersonV1("Marianne V1");
+    }
+
+    @GetMapping(value = "/param", params = "version=2")
+    public PersonV2 paramPersonV2(){
+        return new PersonV2(new Name("Paulo", "V2"));
+    }
+
+    // header versioning
+
+    @GetMapping(value = "/header", headers = "X-API-VERSION=1")
+    public PersonV1 headerPersonV1(){
+        return new PersonV1("Marianne V1");
+    }
+
+    @GetMapping(value = "/header", headers = "X-API-VERSION=2")
+    public PersonV2 headerPersonV2(){
         return new PersonV2(new Name("Paulo", "V2"));
     }
 }
